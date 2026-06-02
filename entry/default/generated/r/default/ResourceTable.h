6b07bfffa38e58b3f91b74c3b15dafc20ff8736c
zoumitui0722 <zoumirui076921@qq.com>
2026-06-02 18:15:47 +0800

    Add files via upload

---
+++ entry/default/generated/r/default/ResourceTable.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RESOURCE_TABLE_H
+#define RESOURCE_TABLE_H
+
+#include<stdint.h>
+
+namespace OHOS {
+const int32_t STRING_ENTRYABILITY_DESC = 0x01000002;
+const int32_t STRING_ENTRYABILITY_LABEL = 0x01000003;
+const int32_t STRING_APP_NAME = 0x01000000;
+const int32_t STRING_DEFAULTFORM_DESC = 0x01000004;
+const int32_t STRING_DEFAULTFORM_LABEL = 0x01000005;
+const int32_t STRING_MODULE_DESC = 0x01000006;
+const int32_t STRING_PAGE_SHOW = 0x01000055;
+const int32_t STRING_PERSONAL_PRIVACY_PROTOCOL = 0x01000007;
+const int32_t STRING_TASK_ACHIEVEMENT_LEVEL = 0x01000008;
+const int32_t STRING_USER_PROTOCOL = 0x01000009;
+const int32_t STRING_WELCOME_SPAN_FIVE = 0x0100000a;
+const int32_t STRING_WELCOME_SPAN_FOUR = 0x0100000b;
+const int32_t STRING_WELCOME_SPAN_ONE = 0x0100000c;
+const int32_t STRING_WELCOME_SPAN_THREE = 0x0100000d;
+const int32_t STRING_WELCOME_SPAN_TWO = 0x0100000e;
+const int32_t STRING_WELCOME_TITLE = 0x0100000f;
+const int32_t COLOR_DARK_GRAY = 0x01000010;
+const int32_t COLOR_PRIVACY_COLOR = 0x01000011;
+const int32_t COLOR_PROGRESS_NORMAL = 0x01000012;
+const int32_t COLOR_PROGRESS_SEL = 0x01000013;
+const int32_t COLOR_START_WINDOW_BACKGROUND = 0x01000014;
+const int32_t COLOR_TAB_BAR = 0x01000015;
+const int32_t COLOR_TAB_BAR_NORMAL = 0x01000016;
+const int32_t COLOR_TAB_BAR_SEL = 0x01000017;
+const int32_t MEDIA_CUSTOMER = 0x01000018;
+const int32_t MEDIA_APP_ICON = 0x01000001;
+const int32_t MEDIA_BACK = 0x01000019;
+const int32_t MEDIA_BACKGROUND = 0x0100001a;
+const int32_t MEDIA_BEIJIN = 0x0100001b;
+const int32_t MEDIA_BIAO = 0x0100001c;
+const int32_t MEDIA_ENTER = 0x0100001d;
+const int32_t MEDIA_FENLEI_NORMAL = 0x0100001e;
+const int32_t MEDIA_FENLEI_SEL = 0x0100001f;
+const int32_t MEDIA_FOREGROUND = 0x01000020;
+const int32_t MEDIA_GERENZHONXIN_NORMAL = 0x01000021;
+const int32_t MEDIA_GERENZHONXIN_SEL = 0x01000022;
+const int32_t MEDIA_HUANYING = 0x01000023;
+const int32_t MEDIA_IC_AUTO = 0x01000024;
+const int32_t MEDIA_IC_BELLS_O = 0x01000025;
+const int32_t MEDIA_IC_CD = 0x01000026;
+const int32_t MEDIA_IC_COMMENT_O = 0x01000027;
+const int32_t MEDIA_IC_DOWNLOAD_O = 0x01000028;
+const int32_t MEDIA_IC_LIKE = 0x01000029;
+const int32_t MEDIA_IC_MORE = 0x0100002a;
+const int32_t MEDIA_IC_NEXT = 0x0100002b;
+const int32_t MEDIA_IC_PAUSED = 0x0100002c;
+const int32_t MEDIA_IC_PLAY = 0x0100002d;
+const int32_t MEDIA_IC_PREV = 0x0100002e;
+const int32_t MEDIA_IC_PUBLIC_PLAY_LAST = 0x0100002f;
+const int32_t MEDIA_IC_PUBLIC_PLAY_NEXT = 0x01000030;
+const int32_t MEDIA_IC_PUBLIC_PLAY_NORM = 0x01000031;
+const int32_t MEDIA_IC_RANDOM = 0x01000032;
+const int32_t MEDIA_IC_REPEAT = 0x01000033;
+const int32_t MEDIA_IC_SHARE = 0x01000034;
+const int32_t MEDIA_IC_SINGLE_PLAY = 0x01000035;
+const int32_t MEDIA_IC_SONG_LIST = 0x01000036;
+const int32_t MEDIA_IC_STYLUS = 0x01000037;
+const int32_t MEDIA_ICON_LIB_106 = 0x01000038;
+const int32_t MEDIA_ICON_LIB_213 = 0x01000039;
+const int32_t MEDIA_LAYERED_IMAGE = 0x0100003a;
+const int32_t MEDIA_LIST1 = 0x0100003b;
+const int32_t MEDIA_LIST2 = 0x0100003c;
+const int32_t MEDIA_LIST3 = 0x0100003d;
+const int32_t MEDIA_LIST4 = 0x0100003e;
+const int32_t MEDIA_LIST5 = 0x0100003f;
+const int32_t MEDIA_LOGO_2 = 0x01000040;
+const int32_t MEDIA_LUNBO1 = 0x01000041;
+const int32_t MEDIA_LUNBO2 = 0x01000042;
+const int32_t MEDIA_LUNBO3 = 0x01000043;
+const int32_t MEDIA_LUNBO4 = 0x01000044;
+const int32_t MEDIA_LUNBO5 = 0x01000045;
+const int32_t MEDIA_MEIR1 = 0x01000046;
+const int32_t MEDIA_MEIR2 = 0x01000047;
+const int32_t MEDIA_MEIR3 = 0x01000048;
+const int32_t MEDIA_MEIR4 = 0x01000049;
+const int32_t MEDIA_MUSIC_BACKGROUNDIMG2 = 0x0100004a;
+const int32_t MEDIA_MUSIC_ICON2 = 0x0100004b;
+const int32_t MEDIA_QITA = 0x0100004c;
+const int32_t MEDIA_RANDOM_BACKGROUND_5 = 0x0100004d;
+const int32_t MEDIA_SOUSUO = 0x0100004e;
+const int32_t MEDIA_SOUYE_NORMAL = 0x0100004f;
+const int32_t MEDIA_SOUYE_SEL = 0x01000050;
+const int32_t MEDIA_STARTICON = 0x01000051;
+const int32_t PROFILE_BACKUP_CONFIG = 0x01000052;
+const int32_t PROFILE_FORM_CONFIG = 0x01000053;
+const int32_t PROFILE_MAIN_PAGES = 0x01000054;
+}
+#endif
